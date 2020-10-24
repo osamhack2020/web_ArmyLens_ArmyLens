f4ecdaede8f63a814a36c1e3c395def134f38d26
yougunwoo <69908078+yougunwoo@users.noreply.github.com>
2020-10-24 19:32:03 +0900

    앱 ~24일까지 최종

--- APP/App.js
+++ APP/App.js
@@ -3,6 +3,6 @@ import { WebView } from 'react-native-webview';
 
 export default class App extends React.Component {
   render() {
-    return <WebView source={{ uri: 'https://optimistic-wright-b97969.netlify.app/' }} style={{ marginTop: 20 }} />;
+    return <WebView source={{ uri: 'https://optimistic-wright-b97969.netlify.app' }} style={{ marginTop: 20 }} />;
   }
 }
